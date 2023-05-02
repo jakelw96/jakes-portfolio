3e08e4d2979a0d5fae1929a960f34465d52c4401
jakelw96 <jake.lw96@gmail.com>
2023-05-01 23:41:16 -0600

    Working on header for mobile. Currently working on functionality for buttons

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,6 +3,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { NavDropdown } from "react-bootstrap";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import ghIcon from "../../assets/icons/github-icon.png";
 import linkedin from "../../assets/icons/li-logo.png";
 
@@ -26,7 +29,7 @@ function Navigation(props) {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
+          <Nav className="main-nav">
             {" "}
             {/* For normal sized screens */}
             {aboutSelected ? (
@@ -163,20 +166,24 @@ function Navigation(props) {
             )}
             <Nav.Link>
               <span className="icons">
-              <a href="https://github.com/jakelw96">
-                <img className="gh-icon" src={ghIcon} alt="GitHub Profile" />
-              </a>
-            <a href="https://www.linkedin.com/in/jacob-williams-968115206/">
-              <img className="li-icon" src={linkedin} alt="LinkedIn Profile" />
-            </a>
-            </span>
-             </Nav.Link>  
+                <a href="https://github.com/jakelw96">
+                  <img className="gh-icon" src={ghIcon} alt="GitHub Profile" />
+                </a>
+                <a href="https://www.linkedin.com/in/jacob-williams-968115206/">
+                  <img
+                    className="li-icon"
+                    src={linkedin}
+                    alt="LinkedIn Profile"
+                  />
+                </a>
+              </span>
+            </Nav.Link>
           </Nav>
 
           <Nav>
             {" "}
             {/* Used for smaller/mobile screens */}
-            <NavDropdown title="Explore!" drop={"start"} id="dropdown">
+            {/* <NavDropdown title="Explore!" drop={"start"} id="dropdown">
               <NavDropdown.Item id="dropdown-item">
                 {aboutSelected ? (
                   <Nav.Link>
@@ -270,7 +277,80 @@ function Navigation(props) {
                   </Nav.Link>
                 )}
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
+            <div className="drop-button">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              {[DropdownButton].map((DropdownType, idx) => (
+                <DropdownType
+                  as={ButtonGroup}
+                  key={idx}
+                  id={`dropdown-button-drop-${idx}`}
+                  size="lg"
+                  variant="dark"
+                  title="Explore Jake's Portfolio"
+                  className="dropdown"
+                >
+                  <Dropdown.Item eventKey="1">Home</Dropdown.Item>
+                  {projectSelected ? (
+                    <>
+                      <Nav.Link>
+                        <Dropdown.Item
+                          eventKey="2"
+                          className="links-main navActive"
+                          onClick={() => {
+                            setProjectSelected(true);
+                            setAboutSelected(false);
+                            setContactSelected(false);
+                            setResumeSelected(false);
+                          }}
+                        >
+                          Projects
+                        </Dropdown.Item>
+                      </Nav.Link>
+                    </>
+                  ) : (
+                    <>
+                      <Nav.Link>
+                        <Dropdown.Item
+                          eventKey="2"
+                          className="links-main"
+                          onClick={() => {
+                            setProjectSelected(true);
+                            setAboutSelected(false);
+                            setContactSelected(false);
+                            setResumeSelected(false);
+                          }}
+                        >
+                          Projects
+                        </Dropdown.Item>
+                      </Nav.Link>
+                    </>
+                  )}
+
+                  <Dropdown.Item eventKey="3">
+                    Resume & Experience
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="4">Contact Me</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item eventKey="4">
+                    <a href="https://github.com/jakelw96">
+                      <img
+                        className="gh-icon-dropdown"
+                        src={ghIcon}
+                        alt="GitHub Profile"
+                      />
+                    </a>
+                    <a href="https://www.linkedin.com/in/jacob-williams-968115206/">
+                      <img
+                        className="li-icon-dropdown"
+                        src={linkedin}
+                        alt="LinkedIn Profile"
+                      />
+                    </a>
+                  </Dropdown.Item>
+                </DropdownType>
+              ))}
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
