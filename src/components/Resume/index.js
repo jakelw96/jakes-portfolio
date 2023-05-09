3201695527d500ce2f56375872f4079dda2f9797
jakelw96 <jake.lw96@gmail.com>
2023-05-09 00:08:54 -0600

    Working on re-design for resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import resumeFile from "../../assets/files/Jacob Williams Resume.pdf";
+import badgeImg from "../../assets/images/uofu-badge.png";
+import jsImg from "../../assets/images/js-logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -8,9 +10,38 @@ import Col from "react-bootstrap/Col";
 function Resume() {
   return (
     <section className="project">
-      <h1>My Skills</h1>
       <Container>
+        <Row className="project-header-container-main">
+          <Col sm={11}>
+            <h1 className="title">Experience</h1>
+            <p className="sub-text">
+              This page outlines the different programming languages I have
+              experience with, their associated libraries, and other
+              miscellaneous skills. The badge to the right is proof of my
+              certification with the University of Utah. Please click on it to
+              verify my credentials. My resume is also available for download!
+            </p>
+          </Col>
+          <Col sm={1}>
+            <a href="https://www.credly.com/badges/079e1b1c-9e8f-4302-82bf-0918392bfca1">
+              <img src={badgeImg} alt="uofu badge"></img>
+            </a>
+          </Col>
+          <Col>
+            <a href={resumeFile} className="resume-link sec-title" download>
+              Download Resume
+            </a>
+          </Col>
+        </Row>
         <Row>
+          <Col>
+            <img src={jsImg} alt="javascript" className="js-img"></img>
+          </Col>
+          <Col>
+          
+          </Col>
+        </Row>
+        {/* <Row>
           <Col sm={3}>
             <h2 className="list-title">Languages</h2>
             <ul>
@@ -49,11 +80,8 @@ function Resume() {
               <li className="list-items">PostgreSQL</li>
             </ul>
           </Col>
-        </Row>
+        </Row> */}
       </Container>
-      <a href={resumeFile} className="resume-link" download>
-        Download Resume
-      </a>
     </section>
   );
 }
