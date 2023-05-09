dcd27f2b85e77bf7860fe61ec8a10f85a55cfaf2
jakelw96 <jake.lw96@gmail.com>
2023-05-08 23:05:14 -0600

    Completed project page re-design on smaller screens

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -12,7 +12,7 @@ import entertainMeImg from "../../assets/images/entertain-me-img.png";
 function Project() {
   return (
     <Container>
-      <Row className="project-header-container">
+      <Row className="project-header-container-main">
         <Col sm={{ span: 12 }}>
           <h1 className="title">Projects</h1>
           <p className="sub-text">
@@ -23,7 +23,7 @@ function Project() {
       </Row>
       <Row className="project-container">
         <Col className="regular-col">
-          <h1 className="sec-title">EntertainMe 2.0</h1>
+          <h2 className="sec-title">EntertainMe 2.0</h2>
           <p className="sec-sub-text">
             A single-page-application that presents info on a specific movie,
             show, or video game based on certain criteria a user enters.
@@ -156,7 +156,7 @@ function Project() {
           </Row>
         </Col>
       </Row>
-      <Row>
+      <Row className="project-container">
         <Col className="regular-col">
           <h1 className="sec-title">Photo Port</h1>
           <p className="sec-sub-text">
@@ -186,6 +186,174 @@ function Project() {
             className="project-img"
             alt="portfolio-phot"
           ></img>
+        </Col>
+      </Row>
+
+      {/* For smaller screens */}
+      <Row className="project-header-container-sub">
+        <Col sm={{ span: 12 }}>
+          <h1 className="title">Projects</h1>
+          <p className="sub-text">
+            These are some of the projects I have made. Please check out my
+            GitHub page to see more of my work!
+          </p>
+        </Col>
+      </Row>
+      <Row className="project-container-sub">
+        <Col sm={{ span: 12 }} className="mobile-col">
+          <h1 className="sec-title">EntertainMe 2.0</h1>
+          <p className="sec-sub-text">
+            A single-page-application that presents info on a specific movie,
+            show, or video game based on certain criteria a user enters.
+            Behaving like a multi-page app, users can efficiently sort through
+            media, save what they are interested in, and share their playlists
+            with friends and fellow users.
+          </p>
+          <Row className="mobile-btn-row">
+            <Col>
+              <a href="https://entertain-me-redux.herokuapp.com/">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">Live Application</h2>
+                </button>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://github.com/jakelw96/entertain-me-redux">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">GitHub Repository</h2>
+                </button>
+              </a>
+            </Col>
+            <img
+              src={entertainMeImg}
+              className="project-img"
+              alt="entertain me"
+            ></img>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="project-container-sub">
+        <Col sm={{ span: 12 }} className="mobile-col">
+          <h1 className="sec-title">Dinner Party</h1>
+          <p className="sec-sub-text">
+            Dinner Party is a place to talk about the things you like and find
+            more things to love.
+          </p>
+          <Row className="mobile-btn-row">
+            <Col>
+              <a href="https://dinner-parties.herokuapp.com/">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">Live Application</h2>
+                </button>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://github.com/jakelw96/dinner-party">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">GitHub Repository</h2>
+                </button>
+              </a>
+            </Col>
+            <img
+              src={dinnerPartyImg}
+              className="project-img"
+              alt="dinner party"
+            ></img>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="project-container-sub">
+        <Col sm={{ span: 12 }} className="mobile-col">
+          <h1 className="sec-title">TheBestSellers</h1>
+          <p className="sec-sub-text">
+            A static HTML page that has the user select one of several current
+            bestsellers lists from the New York Times bestsellers lists and
+            displays the top 6 books in those categories. There is an Amazon
+            link the user can click on to purchase the book, and a save
+            button(using LocalStorage) if the user would like to save the Amazon
+            link while they check different lists.
+          </p>
+          <Row className="mobile-btn-row">
+            <Col>
+              <a href="https://jakelw96.github.io/the-best-sellers/">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">Live Application</h2>
+                </button>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://github.com/jakelw96/the-best-sellers">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">GitHub Repository</h2>
+                </button>
+              </a>
+            </Col>
+            <img
+              src={bestSellersImg}
+              className="project-img"
+              alt="the best sellers"
+            ></img>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="project-container-sub">
+        <Col sm={{ span: 12 }} className="mobile-col">
+          <h1 className="sec-title">Pizza Hunt</h1>
+          <p className="sec-sub-text">
+            Pizza Hunt is an application for users to create their own custom
+            pizza creations. Other users may also comment on those creations.
+          </p>
+          <Row className="mobile-btn-row">
+            <Col>
+              <a href="https://pizza-hunt-jlw.herokuapp.com/">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">Live Application</h2>
+                </button>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://github.com/jakelw96/pizza-hunt">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">GitHub Repository</h2>
+                </button>
+              </a>
+            </Col>
+            <img
+              src={pizzaHuntImg}
+              className="project-img"
+              alt="pizza hunt"
+            ></img>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="project-container-sub">
+        <Col sm={{ span: 12 }} className="mobile-col">
+          <h1 className="sec-title">Photo Port</h1>
+          <p className="sec-sub-text">
+            Photo Port is a practice application for React. It is a portfolio
+            for a collection of photos a user may use to showcase their work.
+          </p>
+          <Row className="mobile-btn-row">
+            <Col>
+              <a href="https://jakelw96.github.io/photo-port/">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">Live Application</h2>
+                </button>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://github.com/jakelw96/photo-port">
+                <button className="projectPageBtn">
+                  <h2 className="projectPageBtnText">GitHub Repository</h2>
+                </button>
+              </a>
+            </Col>
+            <img
+              src={photoPortImg}
+              className="project-img"
+              alt="portfolio-phot"
+            ></img>
+          </Row>
         </Col>
       </Row>
     </Container>
